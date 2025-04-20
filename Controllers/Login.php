7a3d386071954808fa6b208c16576035acf9904a
phatgia <88992394+phatgia@users.noreply.github.com>
2025-04-20 20:30:56 +0700

    Add files via upload

--- Controllers/Login.php
+++ Controllers/Login.php
@@ -1,7 +1,31 @@
 <?php
-class Login extends Controller{
-    function Show(){
-        $this->view("LoginView",["title"=>"Login"]);
+class Login extends Controller {
+
+    public function __construct() {
+        $this->userModel = $this->model("LoginModel");
+    }
+
+    public function Show() {
+        $this->view("LoginView", ["title" => "Login"]);
+    }
+
+    public function Authenticate() {
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $username = $_POST['username'] ?? '';
+            $password = $_POST['password'] ?? '';
+
+            if ($this->userModel->login_check($username, $password)) {
+                header("Location: /controllers/Home");
+                exit();
+            } else {
+                $this->view("LoginView", [
+                    "title" => "Login",
+                    "error" => "Invalid username or password"
+                ]);
+            }
+        } else {
+            header("Location: ./Show");
+        }
     }
 }
 ?>
